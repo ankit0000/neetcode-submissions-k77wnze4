9e4a826b3e711bcacfe14795c14e13594523e5e9
Ankit Singh Tomar <ast.ankit1910@gmail.com>
2026-04-10 23:28:00 +0530

    Add: max-water-container - submission-2

---
+++ Data Structures & Algorithms/max-water-container/submission-2.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} heights
+     * @return {number}
+     */
+    maxArea(heights) {
+       let l = 0;
+       let r = heights.length -1;
+       let max = 0;
+       while(l<r){
+          max = Math.max(max,Math.min(heights[l],heights[r]) * (r-l));
+          if(heights[l]<heights[r]){
+              l++;
+          }else{
+            r--;
+          }
+       }
+          return max;
+    }
+}
