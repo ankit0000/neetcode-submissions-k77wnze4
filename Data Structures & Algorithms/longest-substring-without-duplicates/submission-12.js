40db1b1ca6ed4b98c40aa791e69a76b471217ad6
Ankit Singh Tomar <ast.ankit1910@gmail.com>
2026-04-22 18:59:22 +0530

    Add: longest-substring-without-duplicates - submission-12

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-12.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    lengthOfLongestSubstring(s) {
+        let i = 0;
+        let j = 0;
+        let freqMap = new Map();
+        let maxLength = 0;
+        while(j<s.length){
+            if(freqMap.has(s[j])){
+                 i = Math.max(i, freqMap.get(s[j]) + 1); 
+                // i = (freqMap.get(s[j]) || 0) + 1;
+        
+            }
+                freqMap.set(s[j], j)
+                maxLength = Math.max(maxLength,j-i+1)
+            
+            j++;
+        }
+
+        return maxLength;
+
+    }
+}
